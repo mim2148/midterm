293298bc94730c3fa7351a94b0321c149b680dfc
mim2148 <mim2148@tc.columbia.edu>
2016-11-07 13:19:32 -0500

    Midterm
    I've made some changes

---
+++ code.js
@@ -0,0 +1,17 @@
+function AnswerQuestion(code){
+ if(code==="bodyPosture"){
+	 alert("Correct Answer!");
+
+ }
+else{
+	alert("Incorrect answer. Please, try again");}
+}
+
+function typeAnswer (){
+var distractor = prompt('Type 1 for distracting the audience; Type 2 for self distraction');
+if (distractor ==='1'){
+	alert("Correct Answer!");
+
+} else {
+	alert("Incorrect answer. Please, try again");}
+}
